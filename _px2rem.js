43ec1389755dcb5018444fbf5ac9ed1a9529f3d8
420343878@qq.com <420343878@qq.com>
2019-12-22 13:27:37 +0800

    优化

---
+++ _px2rem.js
@@ -0,0 +1,23 @@
+/*
+ * @Author: xiaolong.qiu
+ * @Date: 2019-12-19 11:15:16
+ * @LastEditTime : 2019-12-22 13:27:28
+ */
+
+
+
+ const fs = require("fs")
+ exports.px2rem = (path) => {
+    //  if(!fs.existsSync(path)){
+    //     console.error(`cannot find ${path}`)
+    //     return
+    //  }
+
+    // 判断后缀是不是css  scss  less include("css")
+    
+    const r = fs.readFileSync(path)
+    const re = r.toString().replace(/([\d]+)px/ig, i => parseFloat(i) / 100 + "rem"); // 不能局限于 $1 $2 等正则变量
+    fs.writeFileSync(path, re) // 默认值utf8
+    console.log(`${path}文件替换完成`);
+ }
+
--- extension.js
+++ extension.js
@@ -1,27 +1,38 @@
+/*
+ * @Author: xiaolong.qiu
+ * @Date: 2019-12-22 11:59:12
+ * @LastEditTime : 2019-12-22 13:26:04
+ */
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const {px2rem} = require("./_px2rem");
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
+
+
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "px2rem" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+	let disposable = vscode.commands.registerCommand('extension.px2rem', function () {
+		const path = "C:/Users/42034/Desktop/demo1.scss";
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+		try {
+			px2rem(path);
+			vscode.window.showInformationMessage('transfer successfully！');
+		} catch (error) {
+			console.log(error.message);
+			vscode.window.showInformationMessage(error.message);
+		}
+		
 	});
 
 	context.subscriptions.push(disposable);
